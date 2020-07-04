70a2581343f21cf7a12d7766f56920768cc1a919
Naoya Takenami <thriver@imac.local>
2020-07-04 09:56:50 +0900

    feat(create): Create画面追加

---
+++ src/app/create/create-routing.module.ts
@@ -0,0 +1,17 @@
+import { CreateComponent } from './create/create.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CreateRoutingModule { }
