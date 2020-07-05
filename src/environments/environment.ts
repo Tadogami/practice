611fcf09abd044b4af7c0bd3392eba13d1121f38
Naoya Takenami <thriver@imac.local>
2020-07-05 17:43:10 +0900

    feat(firebase): firebase連携

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: 'AIzaSyBo-j78YyyASd_0k3mdoKLWC-B5w5uIW_E',
+  authDomain: 'practice-23740.firebaseapp.com',
+  databaseURL: 'https://practice-23740.firebaseio.com',
+  projectId: 'practice-23740',
+  storageBucket: 'practice-23740.appspot.com',
+  messagingSenderId: '297770788468',
+  appId: '1:297770788468:web:6feb3be3377325c7819321',
+  measurementId: 'G-7K4873LQ6S',
+  },
 };
 
 /*
