374326178994269682ab0dfdde4db6ec896d62d2
Naoya Takenami <thriver@imac.local>
2020-07-05 17:05:37 +0900

    feat(home): Petコンポーネント追加

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,16 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PetComponent } from './pet/pet.component';
 
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+
 
 @NgModule({
   declarations: [HomeComponent, PetComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatProgressBarModule,
   ]
 })
 export class HomeModule { }
--- src/app/home/pet/pet.component.ts
+++ src/app/home/pet/pet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Pet } from './../../interfaces/pet';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pet',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetComponent implements OnInit {
 
+  @Input() pet: Pet;
+
+  maxExp = 400;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getExpPercentage(): number {
+    return this.pet.exp / this.maxExp * 100;
+  }
+
 
 }
